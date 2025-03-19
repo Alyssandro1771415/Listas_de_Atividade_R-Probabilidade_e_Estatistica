3c174a3524a35e79a5f440093f32c94532a7e4d8
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2025-03-19 11:29:25 -0300

    feat: concluding the first activitie list

--- Lista1/Lista1.r
+++ Lista1/Lista1.r
@@ -115,11 +115,67 @@ hist(Dados_Funcionarios$Salario,
 # 6.) Considerando as variáveis estado civil (Est.civil) e grau de instrução (Inst):
 #a. Faça uma tabela de contingência para resumir o cruzamento destas duas variáveis
 tabela_contigencia_estadoCivil_grauInstrucao <- table(Dados_Funcionarios$Est.civil,
-                                                      Dados_Funcionarios$Salario)
+                                                      Dados_Funcionarios$Inst)
 print(tabela_contigencia_estadoCivil_grauInstrucao)
 
 #b. A partir da tabela de contingência, faça um gráfico de barras 
 #(veja como fica as barras sobrepostas e lado a lado)
 
+barplot(tabela_contigencia_estadoCivil_grauInstrucao,
+        beside = FALSE,
+        main = "Gráfico de Cruzamento do Estado Civil e Grau de Instrução Sobrepostos",
+        xlab = "Grau de Instrução",
+        ylab = "Frequência")
+barplot(tabela_contigencia_estadoCivil_grauInstrucao,
+        beside = TRUE,
+        main = "Gráfico de Cruzamento do Estado Civil e Grau de Instrução Lado a Lado",
+        xlab = "Grau de Instrução",
+        ylab = "Frequência")
 
 
+
+
+
+#7.) Considerando as variáveis grau de instrução (Inst) e salário (Salario): 
+#a. Faça um box-plot para verificar a relação entre estas duas variáveis
+boxplot(Dados_Funcionarios$Salario ~ Dados_Funcionarios$Inst,
+        main = "Distribuição Salarial por Grau de Instrução",
+        xlab = "Grau de Instrução",
+        ylab = "Salário",
+        col = "lightblue",
+        notch = FALSE)
+
+#b. Calcule a média e o desvio padrão dos salários para cada grau de instrução
+media_salaria_por_grau_instrucao <- tapply(Dados_Funcionarios$Salario, Dados_Funcionarios$Inst,
+                                           mean,
+                                           na.rm=TRUE) 
+print(media_salaria_por_grau_instrucao)
+
+desvio_padrao_por_grau_instrucao <- tapply(Dados_Funcionarios$Salario, Dados_Funcionarios$Inst,
+                                           sd,
+                                           na.rm=TRUE) 
+print(desvio_padrao_por_grau_instrucao)
+
+
+
+
+
+#8.) Considerando as variáveis Anos e Meses:
+#a. Calcule a idade como Anos+Meses/12(criando uma nova coluna chamada Idade)
+Dados_Funcionarios$Idade <- Dados_Funcionarios$Anos+(Dados_Funcionarios$Meses/12) 
+head(Dados_Funcionarios)
+
+
+
+
+
+#9.) Considerando as variáveis Salario e Idade:
+#a. Faça um gráfico para explorar a relação entre estas duas variáveis
+plot(Dados_Funcionarios$Salario ~ Dados_Funcionarios$Idade,
+        main = "Relação entre Salário e Idade",
+        xlab = "Salário",
+        ylab = "Idade")
+
+#b. Calcule a correlação entre estas duas variáveis
+correlacao <- cor(Dados_Funcionarios$Idade, Dados_Funcionarios$Salario, use = "complete.obs")
+print(correlacao)
